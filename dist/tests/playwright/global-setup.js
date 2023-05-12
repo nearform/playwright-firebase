646b21bb9cb6e31949d1f5da785ec628b0318ec6
Ademsk1 <adam.illoul24@gmail.com>
2023-05-12 10:34:41 +0100

    fix: eslint+cleanup setup

--- dist/tests/playwright/global-setup.js
+++ dist/tests/playwright/global-setup.js
@@ -1,18 +1,13 @@
 import { readFileSync } from "fs";
 import { playwrightFirebasePlugin } from "../../index.js";
 import dotenv from 'dotenv';
-import { chromium } from "@playwright/test";
 dotenv.config({ path: '.env', override: true });
 const UID = process.env.UID;
 const serviceAccount = JSON.parse(readFileSync('./serviceAccount.json').toString());
 const OPTIONS = process.env.FIREBASE_CONFIG;
 export default async function globalSetup(config) {
     const { baseURL } = config.projects[0].use;
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
     if (baseURL) {
-        await page.goto(baseURL);
-        console.log('GONE TO PAGE');
         await playwrightFirebasePlugin(UID, serviceAccount, JSON.parse(OPTIONS));
     }
 }
--- tests/playwright/global-setup.ts
+++ tests/playwright/global-setup.ts
@@ -1,3 +1,4 @@
+/* eslint no-non-null-assertion: 0 */
 import { ServiceAccount } from "firebase-admin";
 import { readFileSync } from "fs";
 import { playwrightFirebasePlugin } from "../../index.js";
@@ -10,11 +11,7 @@ const OPTIONS = process.env.FIREBASE_CONFIG!
 
 export default async function globalSetup(config: FullConfig) {
     const { baseURL } = config.projects[0].use
-    const browser = await chromium.launch()
-    const page = await browser.newPage()
     if (baseURL) {
-        await page.goto(baseURL)
-        console.log('GONE TO PAGE')
         await playwrightFirebasePlugin(UID, serviceAccount, JSON.parse(OPTIONS))
     }
 }
