d0966c62d826a25f408dbc118275bf0072d0b53c
Steve Goode <steve.goode@nearform.com>
2023-08-18 10:51:22 +0100

    fix: removed testing site

--- tests/helpers/test-site/auth.setup.ts
+++
@@ -1,8 +0,0 @@
-import playwrightFirebasePlugin from "../../../index";
-import { test as base } from '@playwright/test'
-
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT!)
-const uid = process.env.REACT_APP_UID!
-const options = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG!)
-export const test = playwrightFirebasePlugin(serviceAccount, options, uid, base)
-
